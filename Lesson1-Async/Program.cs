8ce038e95f9fc99bb8f88747c5aa14a26d6e770c
LeoZhw <529409399@qq.com>
2020-04-05 20:45:45 +0800

    build solution

---
+++ Lesson1-Async/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1_Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
